657888a9b8cf10553b3e9639de97aad1de624149
Mark Freedman <mark.freedman@gmail.com>
2020-07-18 17:00:13 -0400

    more stuff

--- src/main/kotlin/FuncTesterLesson3Q13.kt
+++ src/main/kotlin/FuncTesterLesson3Q13.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
     println("hello")
+    whatShouldIDoToday("sad")
+}
+
+
+fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperatureC: Int = 24) : String {
+    return when {
+        mood == "happy" && weather == "Sunny" -> "go for a walk"
+        else -> "Stay home and read."
+
+    }
 }
